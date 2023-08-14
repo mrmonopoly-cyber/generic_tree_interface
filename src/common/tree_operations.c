f911c31c0bf4fdc97dc9321dae9c430a71d3439d
mrmonopoly-cyber <alberto.damo@gmail.com>
2023-08-14 16:21:23 +0200

    testing features and added in_order_visit

--- src/common/tree_operations.c
+++ src/common/tree_operations.c
@@ -1,19 +1,33 @@
-
-
+#include <stdlib.h>
 
 #include "tree_operations.h"
+//private
 
-struct tree_operations {
-  int (*compare_key) (void *,void*);
-  void (*free_data) (void *);
+struct common_node {
+  struct commond_node *children;
+  void *key;
 };
-
-
-tree_operations *create_environment(int (*compare_key) (void *,void*),
-                                    void (*free_data) (void *))
+//functions
+static void swap_keys(void ** key1,void **key2)
 {
-  tree_operations *ops=malloc(sizeof(*ops));
-  ops->compare_key=compare_key;
-  ops->free_data=free_data;
+  void *temp=*key1;
+  key1=key2;
+  key2=&temp;
 }
 
+static void in_order_visit_bin(void *root)
+{
+  
+}
+//public
+struct tree_operations *create_environment(int (*compare_key) (void *,void*),
+                                    void (*free_data) (void *),
+                                    void (*print_key) (void *))
+{
+  struct tree_operations *ops=malloc(sizeof(*ops));
+  ops->compare_key=compare_key;
+  ops->free_data=free_data;
+  ops->swap_keys=swap_keys;
+  ops->print_key=print_key;
+  return ops;
+}
--- src/common/tree_operations.h
+++ src/common/tree_operations.h
@@ -1,8 +1,16 @@
-#ifdef _TREE_OPERATIONS_
+#ifndef _TREE_OPERATIONS_
+#define _TREE_OPERARIONS_
 
-typedef struct tree_operations;
+typedef struct tree_operations tree_operations;
+struct tree_operations {
+  int (*compare_key) (void *,void*);
+  void (*free_data) (void *);
+  void (*swap_keys) (void **,void **);
+  void (*print_key) (void *);
+};
 
 tree_operations *create_environment(int (*compare_key) (void *,void*),
-                                    void (*free_data) (void *));
+                                    void (*free_data) (void *),
+                                    void (*print_key) (void *));
 
 #endif // _TREE_OPERATIONS_
