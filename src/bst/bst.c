91dbc7326109d6f3326ce1b30742cf6addf958b4
mrmonopoly-cyber <alberto.damo@gmail.com>
2023-08-14 16:20:44 +0200

    testing features and added in_order_visit

--- src/bst/bst.c
+++ src/bst/bst.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 
-#include "../common/tree_operations.h"
 #include "bst.h"
 
 //private
@@ -8,96 +7,91 @@
 //data structure 
 struct bst{
   void *key;
-  bst childs[2];
-  bst *parent;
+  struct bst **children;
   struct tree_operations *operations;
 };
 
 //functions
 
-bst *BST_malloc(void *key,bst *parent)
+static bst *BST_malloc(void *key,tree_operations *ops)
 {
   bst *new_node = malloc(sizeof(*new_node));
   new_node->key=key;
-  if(parent!=NULL){
-    new_node->parent=parent;
-  }
-  new_node->childs[0]=NULL;
-  new_node->childs[1]=NULL;
+  new_node->children=malloc(sizeof(*new_node->children));
+  new_node->operations=ops;
   return new_node;
 }
 
-void BST_unlink(bst *parent,bst *to_delete)
-{
-  if(parente->childs[0] != NULL && parente->childs[0] == to_delete){
-    parente->childs[0]=NULL;
-  }else {
-    parente->childs[1]=NULL;
-  }
-}
 //public
-int BST_insert(bst **root,void *key)
+int BST_insert(bst **root,void *key,tree_operations *ops)
 {
   if(*root==NULL){
-    *root=BST_malloc(key,NULL);
+    *root=BST_malloc(key,ops);
     return 0;
   }
 
   int is_greater = (*root)->operations->compare_key((*root)->key,key);
   bst *new_node=NULL;
   if(is_greater >= 0){
-    if((*root)->childs[0] == NULL){
-      (*root)->childs[0]=BST_malloc(key,(*root));
-    }
-    return BST_insert(&(*root)->childs[0],key);
+    return BST_insert(&(*root)->children[1],key,ops);
   }
-  if((*root)->childs[1] == NULL){
-    (*root)->childs[1]=BST_malloc(key,(*root));
-  }
-  return BST_insert(&(*root)->childs[1],key);
+  return BST_insert(&(*root)->children[0],key,ops);
 
   return -1;
 }
 
 int BST_delete(bst **root,void *key)
 {
-  bst *to_delete = BST_search(root,key);
-  bst *to_delete_parent = to_delete->parent;
-  bst *candidate;
+  bst *node = *root;
+  //searching node
+  int is_greater = node->operations->compare_key(node->key,key);
+  if(is_greater > 0){
+    return BST_delete(&node->children[1],key);
+  }else if(is_greater <0){
+    return BST_delete(&node->children[0],key);
+  }
+  //find it
   
   //leaf node
-  if(to_delete->childs[0]==NULL && to_delete->childs[1] == NULL){
-    BST_unlink(to_delete_parent,to_delete);
-    BST_free(to_delete);
+  if(node->children[0]==NULL && node->children[1]==NULL){
+    BST_free(node);
+    *root=NULL;
     return 0;
   }
-  
-  //middle node
-  candidate = (*root)->operations->min((*root)->childs[1]);
-  if(candidate==NULL){
-    candidate = (*root)->operations->max((*root)->childs[0]);
-  }
-  if(candidate !=NULL){
-    (*root)->operations->swap_keys(to_delete,candidate);
-    BST_unlink(candidate->parent,candidate);
-    BST_free(candidate);
+
+  //2 children node
+  if(node->children[0]!=NULL && node->children[1]!=NULL){
+    bst *successor_parent = node;
+    bst *greater_child = node->children[1];
+    bst *successor = greater_child;
+    while (successor->children[0]!=NULL) {
+      successor_parent=successor;
+      successor=successor->children[0];
+    }
+    node->operations->swap_keys(&node->key,&successor->key);
+    if(greater_child == successor){
+      node->children[1]=successor->children[1];
+      successor->children[1]=NULL;
+      BST_free(successor);
+      return 0;
+    }
+    BST_free(successor);
+    successor_parent->children[0]=NULL;
     return 0;
   }
-  // double NULL case, need rotation
-  candidate = (*root)->operations->min((*root));
-  if(candidate==NULL){
-    candidate = (*root)->operations->max((*root));
+
+  //1 child node
+  bst *child =node->children[0];
+  if(node->children[0]==NULL){
+    child=node->children[1];
   }
-  (*root)->operations->swap_keys(candidate->parent,candidate);
-  bst *candidate_parent_childs = candidate->parente->childs;
-  if(candidate_parent_childs[0]!=NULL && candidate_parent_childs[0]==candidate){
-    candidate_parent_childs[1]=candidate;
-    candidate_parent_childs[0]=NULL;
-  }else {
-    candidate_parent_childs[0]=candidate;
-    candidate_parent_childs[1]=NULL;
-  }
-  BST_delete(&to_delete,key);
+  node->operations->swap_keys(&node->key,&child->key);
+  node->children[0]=child->children[0];
+  node->children[1]=child->children[1];
+  child->children[0]=NULL;
+  child->children[1]=NULL;
+  BST_free(child);
+  //error 
   return 0;
 }
 
@@ -112,21 +106,21 @@ bst *BST_search(bst **root,void *key)
         return temp;
         break;
       case 1:
-        return BST_search(&temp->childs[0],key);
+        return BST_search(&temp->children[0],key);
         break;
       case -1:
-        return BST_search(&temp->childs[1],key);
+        return BST_search(&temp->children[1],key);
         break;
     }
   }
   return NULL;
 }
 
-void BST_free(bst **root)
+void BST_free(bst *root)
 {
   if(root!=NULL){
-    BST_free(root->childs[0]);
-    BST_free(root->childs[1]);
+    BST_free(root->children[0]);
+    BST_free(root->children[1]);
 
     if(root->key!=NULL && root->operations->free_data!=NULL){
       root->operations->free_data(root->key);
@@ -138,7 +132,18 @@ void BST_free(bst **root)
 }
 
 
+void BST_in_order_visit(bst *root)
+{
+  if(root!=NULL){
+    BST_in_order_visit(root->children[0]);
+    void *key=root->key;
+    root->operations->print_key(key);
+    BST_in_order_visit(root->children[1]);
+  }
+}
 //debug
+
+#include <stdio.h>
 int compare_key(void *data1,void *data2)
 {
   int int_data1 = *(int *) data1;
@@ -146,21 +151,46 @@ int compare_key(void *data1,void *data2)
   if(int_data1==int_data2){
     return 0;
   }
-  if(int_data1<int_data2){
+  if(int_data1>int_data2){
     return -1;
   }
   return 1;
 }
 
-
+void print_key(void *key)
+{
+  int dig = *(int *)key;
+  printf("%d\n",dig);
+}
 
 int main(int argc, char *argv[])
 {
   tree_operations *env;
-  env = create_environment(compare_key,NULL);
+  bst *root=NULL;
+
+  env = create_environment(compare_key,NULL,print_key);
   int key1 = 1;
   int key2 = 2;
-  
+  int key4 = 4;
+
+  printf("empty tree\n");
+  BST_in_order_visit(root);
+  printf("with one insertion\n");
+  BST_insert(&root,(void *) &key1,env);
+  BST_in_order_visit(root);
+
+  printf("with two insertion\n");
+  BST_insert(&root,(void *) &key2,env);
+  BST_in_order_visit(root);
+
+  printf("with one instertion and one deletion insertion\n");
+  BST_delete(&root,(void *)&key2);
+  BST_in_order_visit(root);
+
+  printf("reinsertion after deliting\n");
+  BST_insert(&root,(void *) &key4,env);
+  BST_in_order_visit(root);
+
   
   return EXIT_SUCCESS;
 }
--- src/bst/bst.h
+++ src/bst/bst.h
@@ -1,12 +1,13 @@
 #ifndef _BST_TREE_
 #define _BST_TREE_
-
+#include "tree_operations.h"
 
 typedef struct bst bst;
 
-int BST_insert(bst **root,void *key);
+int BST_insert(bst **root,void *key,tree_operations *ops);
 int BST_delete(bst **root,void *key);
 bst *BST_search(bst **root,void *key);
+void BST_in_order_visit(bst *root);
 
 void BST_free(bst *root);
 #endif // !_BST_TREE_
