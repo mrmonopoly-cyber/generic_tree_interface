00b112eb5f6e8a15f731ea6bcd486e033ed80623
mrmonopoly-cyber <alberto.damo@gmail.com>
2023-08-18 17:06:57 +0200

    test_bst.c improved test bst

--- tests/bst/test_bst.c
+++ tests/bst/test_bst.c
@@ -36,14 +36,21 @@ int main(int argc, char *argv[])
     BST_insert(&root,(void *) &dati[i],env);
   }
   printf("dopo tutti gli inserimenti\n");
+  printf("pre_order visit\n");
   BST_pre_order_visit(root);
+  printf("in_order visit\n");
+  BST_in_order_visit(root);
 
   //delete
   for (int j=0;j<rm_len;++j) {
     BST_delete(&root,(void *)&rm[j]);
+    printf("eliminato %d\n",rm[j]);
   }
   printf("dopo tutte le eliminazioni\n");
+  printf("pre_order visit\n");
   BST_pre_order_visit(root);
+  printf("in_order visit\n");
+  BST_in_order_visit(root);
   
   BST_free(root);
   root=NULL;
