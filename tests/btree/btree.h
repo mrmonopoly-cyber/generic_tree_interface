7b597c13a12646fc4d7e3f3dcfa8d9b06b15aa7c
mrmonopoly-cyber <alberto.damo@gmail.com>
2023-09-01 10:39:31 +0200

    readding skeleton of btree

---
+++ tests/btree/btree.h
@@ -0,0 +1,1 @@
+../../src/btree/btree.h
---
+++ tests/btree/test_btree.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include "btree.h"
+
+int main(void)
+{
+  tree_operations *env;
+  btree *root=NULL;
+  int dati[] = {};
+  int num_el = sizeof(dati) / sizeof(dati[0]);
+  int rm[] = {};
+  int rm_len = sizeof(rm) / sizeof(rm[0]);
+  env = BTREE_environment(NULL,NULL);
+  
+  //insert
+  for (int i=0;i<num_el;++i) {
+    BTREE_insert(&root,(void *) &dati[i],env);
+  }
+  printf("dopo tutti gli inserimenti\n");
+  printf("pre_order visit\n");
+  BTREE_pre_order_visit(root);
+  printf("in_order visit\n");
+  BTREE_in_order_visit(root);
+
+  //delete
+  for (int j=0;j<rm_len;++j) {
+    BTREE_delete(&root,(void *)&rm[j]);
+    printf("eliminato %d\n",rm[j]);
+  }
+  printf("dopo tutte le eliminazioni\n");
+  printf("pre_order visit\n");
+  BTREE_pre_order_visit(root);
+  printf("in_order visit\n");
+  BTREE_in_order_visit(root);
+  
+  BTREE_free(root);
+  root=NULL;
+  BTREE_pre_order_visit(root);
+  return 0;
+}
